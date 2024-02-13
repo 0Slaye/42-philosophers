751a36d3b863352da5e413ef7532b44eecf644d0
0Slaye <uduterrage@gmail.com>
2024-02-13 18:36:27 +0100

    Fix: norminette

--- philosophers/sources/philosophers/threads.c
+++ philosophers/sources/philosophers/threads.c
@@ -6,7 +6,7 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:31:13 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/02/13 16:56:17 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/02/13 18:36:00 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,15 @@ void	pthreading(t_philosopher **philosophers, int size, void *p_routine)
 	while (++i < size)
 	{
 		if (pthread_create(&threads[i], NULL, p_routine, philosophers[i]) != 0)
-			return (philosopher_free(philosophers, size), ft_putstr_fd("pthread: error.\n", 2));
+			return (philosopher_free(philosophers, size), \
+		ft_putstr_fd("pthread: error.\n", 2));
 	}
 	i = -1;
 	while (++i < size)
 	{
 		if (pthread_join(threads[i], NULL) != 0)
-			return (philosopher_free(philosophers, size), ft_putstr_fd("pthread: error.\n", 2));
+			return (philosopher_free(philosophers, size), \
+		ft_putstr_fd("pthread: error.\n", 2));
 	}
 	philosopher_free(philosophers, size);
 	free(threads);

6973ee49a31637ad19adc1b91c91b1f6fd39415a
0Slaye <uduterrage@gmail.com>
2024-02-19 12:13:38 +0100

    Add: checking for args

--- philosophers/sources/main.c
+++ philosophers/sources/main.c
@@ -6,16 +6,36 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 16:07:44 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/02/17 15:38:12 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/02/19 12:11:51 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commons.h"
 
+int	check_arg(int argc, char **argv)
+{
+	if (ft_atol(argv[1]) <= 0)
+		return (0);
+	else if (ft_atol(argv[2]) <= 0)
+		return (0);
+	else if (ft_atol(argv[3]) <= 0)
+		return (0);
+	else if (ft_atol(argv[4]) <= 0)
+		return (0);
+	if (argc == 6)
+	{
+		if (ft_atol(argv[5]) <= 0)
+			return (0);
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc < 5 || argc > 6)
 		return (0);
+	if (!check_arg(argc, argv))
+		return (ft_error("error: args\n"), 0);
 	philosophers(argc, argv);
 	return (0);
 }
